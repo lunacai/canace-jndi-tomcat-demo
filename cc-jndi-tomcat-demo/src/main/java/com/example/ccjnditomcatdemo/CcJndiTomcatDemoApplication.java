053a3aed22b0675ac4240be599260b634ed30cba
lunacaishulie.io <lunacaishulie.io>
2020-08-14 16:47:08 +0800

    first commit

---
+++ cc-jndi-tomcat-demo/src/main/java/com/example/ccjnditomcatdemo/CcJndiTomcatDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.ccjnditomcatdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class CcJndiTomcatDemoApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcJndiTomcatDemoApplication.class, args);
+    }
+
+}
---
+++ cc-jndi-tomcat-demo/src/test/java/com/example/ccjnditomcatdemo/CcJndiTomcatDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ccjnditomcatdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcJndiTomcatDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
